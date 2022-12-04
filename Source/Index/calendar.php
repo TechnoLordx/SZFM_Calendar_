e141802b9bb973728fd9a064e231b06a07d369f3
Vass Bálint <vassbalint6366@gmail.com>
2022-12-04 21:20:32 +0100

    Oldalak összekötése, XAMPP törlése

---
+++ Source/Index/calendar.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header("Location: lsindex.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>B1G Project</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./weather_style.css">
+    <script src="./weather_script.js" defer></script>
+</head>
+
+<header>
+    <nav class="navbar">
+		<a href="#" class="logo">CALENDAR</a>
+        <div class="nav_links">
+			<ul>
+                <a href="exit.php" class="btn" id="sign">Sign Out</a>
+			</ul>
+        </div>
+        <img src="menu-btn.png" alt="menu lines" class="menu_lines">
+    </nav>
+    <script type="text/javascript" src="Script/nav.js"></script>
+</header>
+<body>
+    <div id="app"></div>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
